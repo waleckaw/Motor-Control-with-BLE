8f08e35f6569d8a05e81d058b4841c65fa6c7c70
waleckaw <willwalecka@gmail.com>
2020-07-23 13:24:42 -0700

    short implementation of motor script in BLEMCClient. also clean WWsched, which still has lots of nested comments to clarify how it works

--- BLEMCClient.py
+++ BLEMCClient.py
@@ -1,5 +1,7 @@
 # Client - BLE/Motor Control Proj
-# Client lib automatically import as e. available methods to access motor include:
+# Client lib automatically import as e to use from REPL. 
+
+#available methods to access motor include:
 
 # e.client_writeSpeed(30) - input is int between 30 and 70 (rpm)
 # e.client_readSpeed() - prints current speed to terminal
@@ -12,31 +14,35 @@
 # e.client_writeDirex(True) - input is True/False, depending on user circuit
 # e.client_readDirex() - reads direx as 1/0
 
+# scripting interactions also useful, but you should comment out the lines in boot.py
 # example script:
 
-e.client_writeStatus(False)
-utime.sleep_ms(50)
-e.client_writeSpeed(43)
-utime.sleep_ms(50)
-e.client_writeSpeed(30)
-utime.sleep_ms(50)
-e.client_writeDirex(False)
-utime.sleep_ms(50)
-e.client_writeSpeed(43)
-utime.sleep_ms(50)
-e.client_writeDirex(True)
-utime.sleep_ms(50)
-e.client_writeSpeed(30)
-utime.sleep_ms(50)
-e.client_writeDirex(False)
-utime.sleep_ms(50)
-e.client_writeSpeed(43)
-utime.sleep_ms(50)
-e.client_writeDirex(True)
-utime.sleep_ms(50)
-e.client_writeSpeed(30)		
-utime.sleep_ms(50)
-e.client_writeStatus(True)
+from BLE_Class import mc_BLE
+e = mc_BLE()
+def randomFunc():
+	e.client_writeStatus(False)
+	utime.sleep_ms(50)
+	e.client_writeSpeed(43)
+	utime.sleep_ms(50)
+	e.client_writeSpeed(30)
+	utime.sleep_ms(50)
+	e.client_writeDirex(False)
+	utime.sleep_ms(50)
+	e.client_writeSpeed(43)
+	utime.sleep_ms(50)
+	e.client_writeDirex(True)
+	utime.sleep_ms(50)
+	e.client_writeSpeed(30)
+	utime.sleep_ms(50)
+	e.client_writeDirex(False)
+	utime.sleep_ms(50)
+	e.client_writeSpeed(43)
+	utime.sleep_ms(50)
+	e.client_writeDirex(True)
+	utime.sleep_ms(50)
+	e.client_writeSpeed(30)		
+	utime.sleep_ms(50)
+	e.client_writeStatus(True)
 
 
 
--- WWsched.py
+++ WWsched.py
@@ -11,16 +11,17 @@ from micropython import const
 
 micropython.alloc_emergency_exception_buf(100)
 
-#show debug messages
+#show debug messages if 1
 WW_DEBUG = const(0)
 
-#use timing GPIO
+#use timing GPIO if 1
 WW_TIMING = const(1)
 
-#timing task & caallbacks
+#timing task & callbacks
 TIMING_TASK = const(0)
 TIMING_CALLBACK = const(1)
 
+# turns on and off GPIO used to time tasks and flag callbacks on scope (if specified above)
 class TIMING_CONTEXT(object):
 
 	def __init__(self, pin):
@@ -41,24 +42,25 @@ class TIMING_CONTEXT(object):
 ####################################################################################################################
 
 class coopSched:
-	#somehow adding one of these should be like adding something to es framework... run function etc
+	
+	# tasks are imported as objects so that they can be run at regular intervals
+	# interval should be significantly longer than input tick_per_ms
 	class task:
 		def __init__(self, user_task, per_ms):
 			self.taskobj = user_task
-			self.interval = per_ms #watch out to make sure this period is considerably longer than tick_interval
+			self.interval = per_ms
 			self.last_tick = 0
 
+	# Timer defined here always goes off every 1 ms... if you only want sys to run (check all flags) 
+	# at (for example) 50 ms intervals, change coopSched.sys_tick_interval to 50. see run() below
 	def __init__(self, tick_per_ms=1, use_esp32=False):   #1 ms as default period
 		self.track_var = False
 		self.sys_tick_interval=tick_per_ms
-		# self.task_list = []
 		self.task_dict = {}
 		self.task_id_list = []
 		if WW_DEBUG: print('sys tick interval set to ', self.sys_tick_interval);
 		self.tick_count = 0
 		self._timing_pin = Pin(23, Pin.OUT)
-		#Timer defined here always goes off every 1 ms... if you only want sys to run at 50 ms intervals, change coopSched.sys_tick_interval to 50
-		#timer can't go any faster
 		if use_esp32:
 			sys_tick_int_tim=Timer(-1)
 			sys_tick_int_tim.init(mode=Timer.PERIODIC, period=1, callback=self.tick)
@@ -66,45 +68,38 @@ class coopSched:
 			sys_tick_int_tim = Timer(mode=Timer.PERIODIC, period=1, callback=self.tick) #change period back to one
 		self.flag_master_callback_ID_dict = {}
 
-	# def toggleTimingPin(self):
-	# 	if self.timing_pin.value():
-	# 		self.timing_pin.off()
-	# 	else:
-	# 		self.timing_pin.on()
-
+	# increase clock count
 	def tick(self, tym):
 		self.tick_count = self.tick_count+1
 
+	# add task to scheduler
 	def addTask(self, user_task, per_ms):
 		if per_ms < self.sys_tick_interval:
 			raise Exception("task will run AT MOST at the same frequency as the scheduler tick")
 		newTask = coopSched.task(user_task, per_ms)
-		# self.task_list.append(newTask)
 		self.task_dict[user_task.task_id] = newTask
 		self.task_id_list.append(user_task.task_id)
 
+	# for each faalg type, makes list of task ID's of the tasks that post/listen for this flag - comments in code clarify how
 	def __addCallbacksToMasterDict(self):
-		#add all flag callback task ID's to master list
 		for a in range(len(self.task_id_list)):
 			for b in range(len(self.task_dict[self.task_id_list[a]].taskobj.flag_list)):
 				if (self.task_dict[self.task_id_list[a]].taskobj.flag_list[b].getFlagType() not in self.flag_master_callback_ID_dict):
 					# if you find a new type of flag, initialize a callback list for it in the master flag callback dict
 					new_flag_added_ID = self.task_dict[self.task_id_list[a]].taskobj.flag_list[b].getFlagType()
 					self.flag_master_callback_ID_dict[new_flag_added_ID] = []
-					#if you find a new key (flag), go through every task and check if it shares a flag... if so, add new callback to list
+					#if you find a new key (flag), go through every task and check if it shares a flag... if so, add task's taskID to list
 					for c in range(len(self.task_id_list)):
 						for d in range(len(self.task_dict[self.task_id_list[c]].taskobj.flag_list)):
 							if self.task_dict[self.task_id_list[c]].taskobj.flag_list[d].getFlagType() == new_flag_added_ID and self.task_dict[self.task_id_list[c]].taskobj.flag_list[d].flag_callback is not None:
 								self.flag_master_callback_ID_dict[new_flag_added_ID].append(self.task_dict[self.task_id_list[c]].taskobj.task_id)
 		if WW_DEBUG: print("callback dict:", self.flag_master_callback_ID_dict)
 
+	# checks flags every time sys clock reaches number of ms defined by sys_tick_interval
 	def run(self):
-
-		#create list of callbacks for each flag
 
 		if WW_DEBUG: print('task dict: ', self.task_dict)
 		if WW_DEBUG: print('task id list: ', self.task_id_list)
-
 		self.__addCallbacksToMasterDict()
 
 		while(True):
@@ -118,7 +113,7 @@ class coopSched:
 				for a in range(len(self.task_id_list)):
 					for b in range(len(self.task_dict[self.task_id_list[a]].taskobj.flag_list)):
 						curr_flag = self.task_dict[self.task_id_list[a]].taskobj.flag_list[b]
-						#curr_flag = the actual flag in the task that you are checking
+						#curr_flag = the actual flag in the task being checked
 						if curr_flag.flag_set:
 							# if flag is set and hasn't already been called from master callback dict
 							if curr_flag.flag_type not in serviced_flag_list:
@@ -150,10 +145,10 @@ class coopSched:
 												flag_to_service.flag_callback(param_to_use)
 											else:
 												flag_to_service.flag_callback()
-								#This should also ensure that if flag set in other task but already has been checked, unset and ignore
+						# unset flag - above structure (esp. serviced flag list) makes sure no flag callback gets run twice
 						curr_flag.unsetFlag()
 
-				#run ongoing tasks
+				#run ongoing tasks at their specific intervals
 				if WW_DEBUG: print('running tasks');
 				for a in range(len(self.task_id_list)):
 					if ((self.tick_count - self.task_dict[self.task_id_list[a]].last_tick) >= self.task_dict[self.task_id_list[a]].interval):
@@ -171,15 +166,15 @@ class coopSched:
 
 ####################################################################################################################
 
+# flag class contains callback assigned by task, flag type (identifier), flag status (set/unset), 
+# indicator of whether or not caallback takes param, and flag param
 class flag:
 	def __init__(self,fn,ftype,take_param=False):
-		# self.flag_name = name
 		self.flag_type = ftype
 		self.flag_set = False
 		self.flag_callback = fn
 		self.param = None
 		self._take_param = take_param
-		# self.hasParam = False
 
 	def setFlag(self, inp_param=None):
 		self.flag_set = True
